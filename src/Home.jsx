4abb13cd42659ff10d158162f0a16120ca756114
rajatmittal <rajatmittal359@gmail.com>
2025-09-26 00:16:39 +0530

    hghjjhb

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "./features/slice/apislice";
-
+import { useNavigate } from "react-router-dom";
 const Home = () => {
   const dispatch = useDispatch();
-
+const navigate = useNavigate()
   // Access products, status, error from store
   const { products, status, error } = useSelector((state) => state.api);
 
+  const goto =()=>{
+    navigate('fetch')
+  }
   useEffect(() => {
     if (status === "idle") {
       dispatch(fetchProducts());
@@ -16,30 +19,9 @@ const Home = () => {
   }, [status, dispatch]);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Products</h1>
-
-      {status === "loading" && <p>Loading products...</p>}
-      {status === "failed" && <p className="text-red-500">{error}</p>}
-
-      {status === "succeeded" && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="border rounded-lg shadow-md p-4 hover:shadow-lg transition"
-            >
-              <img
-                src={product.image}
-                alt={product.title}
-                className="w-full h-40 object-contain mb-2"
-              />
-              <h2 className="font-semibold text-lg truncate">{product.title}</h2>
-              <p className="text-gray-600">${product.price}</p>
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="flex flex-col justify-center items-center mt-50">
+      
+      <button className="text-2xl font-semibold mb-4 " onClick={goto}><u>Shop Now</u></button>
     </div>
   );
 };
