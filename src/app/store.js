48adc89ebe632e776f73bb6552aa01a75cc2b20b
rajatmittal <rajatmittal359@gmail.com>
2025-09-25 13:03:10 +0530

    second

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../features/slice/cartSlice'
-import authReducer from '../features/slice/authSlice'
+import authReducer from "../features/slice/authslice";
 import apiReducer from '../features/slice/apislice'
 import checkoutReducer from "../features/slice/checkoutSlice";
 export const store = configureStore({
